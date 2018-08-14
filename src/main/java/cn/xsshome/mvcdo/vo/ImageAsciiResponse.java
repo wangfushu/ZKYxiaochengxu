e602d76f6b5910dec2808abc8cbc5cb2117a4369
小帅丶 <578274480@qq.com>
2018-08-14 21:38:43 +0800

    :sparkles:新增图片转字符图片功能

---
+++ src/main/java/cn/xsshome/mvcdo/vo/ImageAsciiResponse.java
@@ -0,0 +1,19 @@
+package cn.xsshome.mvcdo.vo;
+/**
+ * 
+ * @author 小帅丶
+ * @date 2018年8月14日
+ * <p>Description: 图片转字符页面对象</p>
+ */
+public class ImageAsciiResponse extends RestResponse{
+	private String image;
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+}
